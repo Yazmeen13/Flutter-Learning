ad0963b7a0af1e1bf9751b90b347cac244f50bfd
Yazmeen <yazmeen131@gmail.com>
2024-09-17 01:04:19 -0400

    done for the night

---
+++ adv_basics/lib/data/questions.dart
@@ -0,0 +1,12 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'what are the main ',
+    [
+      '1',
+      '2',
+      '3',
+    ],
+  ),
+];
