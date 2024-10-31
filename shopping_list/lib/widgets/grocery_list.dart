3123440327451e33df9064d5df91fee5437bba98
Yazmeen Imam <yazmeenimam@gmail.com>
2024-10-31 15:06:10 -0400

    added new item form

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem(){
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => const NewItem()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +22,9 @@ class GroceryList extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Groceries'),
         backgroundColor: Theme.of(context).colorScheme.surface,
+        actions: [
+          IconButton(onPressed: _addItem, icon: const Icon(Icons.add)),
+        ],
       ),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: ListView.builder(
